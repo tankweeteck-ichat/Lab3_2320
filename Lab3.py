0866561fccb7501403acd35b59613b0daae70608
Kwee Teck <tan_kwee_teck@ichat.sp.edu.sg>
2023-10-30 02:35:27 +0800

    Modified Test_Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -12,7 +12,13 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    if n < 10 and n != 0:
+        # Check that all values are integers.
+        check_result = all(isinstance(ele, int) for ele in arr)
+        if check_result==False:
+            arr_result = 2
+            return arr_result
+
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,8 +39,11 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
+    elif n==0:
+        arr_result = 0
+
     else:
-        arr_result = -1
+        arr_result = 1
 
     return arr_result
 
--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,33 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+
+def test_bubble_sort_more_than_10():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 55, 66, 77, 88]
+    expected_result = 1
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == expected_result)
+
+
+def test_bubble_sort_no_number():
+    result = []
+    input_arr = []
+    expected_result = 0
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == expected_result)
+
+
+def test_bubble_sort_not_all_integers():
+    result = []
+    input_arr = [64, 34, 25, 12, 22.5, 11, 90]
+    expected_result = 2
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == expected_result)
